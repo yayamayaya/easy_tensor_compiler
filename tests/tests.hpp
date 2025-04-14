6da577e004714fcda63b5f538c95082a825d5bc8
yayamayaya <semenov.aleksandr@phystech.edu>
2025-04-14 16:07:35 +0300

    добавил тесты для арифметических операций

--- tests/tests.hpp
+++ tests/tests.hpp
@@ -4,45 +4,84 @@
 #include <gtest/gtest.h>
 #include "tensor_compiler.hpp"
 
-TEST(tensor_testing, tensor_init)
-{
-    tensor t(1, 1, 2, 3);
-
-    t = {1, 2, 3, 3, 2, 1};
-
-    EXPECT_NO_THROW(t(0, 0, 1, 2));
-}
-
 TEST(tensor_testing, tensor_print)
 {
-    tensor t(1, 1, 2, 2);
+    tensor t({1, 1, 2, 2});
+    t.set_tensor_size(1, 1, 2, 2);
 
-    t = std::vector<number_t>{1, 2, 3, 4};
+    tensor t2({1, 2, 3, 4, 4, 4, 4, 5, 61, 21, 2, 3, 10, 9, 8, 7});
+    t2.set_tensor_size(2, 4, 1, 2);
 
     std::cout << t << std::endl;
 }
 
-TEST(tensor_testing, out_of_range_ind)
+TEST(tensor_testing, index_test)
 {
-    tensor t(1, 1, 2, 2);
+    tensor t1({1, 1, 1});
+    t1.set_tensor_size(1, 1, 3, 1);
+    EXPECT_NO_THROW(t1(0, 0, 1, 0));
 
-    EXPECT_ANY_THROW(t(2, 2, 2, 2));
+    tensor t2({1, 1, 2, 2});
+    t2.set_tensor_size(1, 1, 2, 2);
+    EXPECT_ANY_THROW(t2(2, 2, 2, 2));
+
+    tensor t3({2, 1, 4, 3});
+    EXPECT_ANY_THROW(t2(1, 1, 1, 1));
 }
 
-TEST(math_testing, scalar_adds_op)
+TEST(tensor_testing, scalar_add)
 {
-    tensor t1(1, 1, 1, 2);
-    tensor t2(1, 1, 1, 2);
+    tensor r({1, 2, 3});
+    r.set_tensor_size(1, 1, 1, 3);
 
-    t1 = {5, 6};
-    t2 = {3, 5};
+    tensor l({3, 2, 1});
+    l.set_tensor_size(1, 1, 1, 3);
 
-    _LOG << "first tensor:  " << t1 << END_;
-    _LOG << "second tensor: " << t2 << END_;
+    tensor res({4, 4, 4});
+    res.set_tensor_size(1, 1, 1, 3);
 
-    _LOG << "sum result: " << t1 + t2 << END_;
+    EXPECT_EQ(r + l, res);
 
-    // const auto &input = std::make_shared<inе>
+    tensor r2({1, 1, 1});
+    r2.set_tensor_size(1, 1, 1, 3);
+
+    tensor l2({1, 1, 1});
+    l2.set_tensor_size(1, 1, 3, 1);
+
+    EXPECT_ANY_THROW(l2 + r2);
+}
+
+TEST(tensor_testing, scalar_mul)
+{
+    tensor r({1, 2, 3});
+    r.set_tensor_size(1, 1, 3, 1);
+
+    tensor res({2, 4, 6});
+    res.set_tensor_size(1, 1, 3, 1);
+
+    EXPECT_EQ(r * 2, res);
+}
+
+TEST(tensor_testing, mat_mul)
+{
+    tensor l1({1, 2, 3});
+    l1.set_tensor_size(1, 1, 1, 3);
+
+    tensor r1({4, 5, 6});
+    r1.set_tensor_size(1, 1, 3, 1);
+
+    tensor res({32});
+    res.set_tensor_size(1, 1, 1, 1);
+
+    EXPECT_EQ(l1 * r1, res);
+
+    tensor l2({1, 2, 3});
+    l2.set_tensor_size(1, 1, 1, 3);
+
+    tensor r2({1, 2, 3});
+    r2.set_tensor_size(1, 1, 1, 3);
+
+    EXPECT_ANY_THROW(l2 * r2);
 }
 
 #endif
