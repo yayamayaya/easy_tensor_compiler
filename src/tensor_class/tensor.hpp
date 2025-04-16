0311b9b75526c30005883c2960013514b059c476
yayamayaya <semenov.aleksandr@phystech.edu>
2025-04-16 19:43:53 +0300

    изменил конструктор тензора, теперь всегда нужно передавать размер тензору при его создании

--- src/tensor_class/tensor.hpp
+++ src/tensor_class/tensor.hpp
@@ -32,8 +32,9 @@ class tensor
 {
 public:
 
-    //Create tensor with data
-    tensor(std::vector<number_t> numbers): data(numbers), size() {};
+    //Create tensor with data and given size
+    tensor(const size_t N, const size_t C, const size_t H, const size_t W,\
+            std::vector<number_t> numbers): data(numbers), size(N, C, H, W) {};
 
     tensor() =delete;
 
@@ -69,14 +70,20 @@ public:
     bool   operator ==(const tensor& rhs) const;
     
 private:
-    
+
     std::vector<number_t> data;
 
     tensor_dim size;
 
     tensor_dim get_size()  const;
 
+// #ifdef OPTIMIZED_TENSORS
+    tensor transpose(const tensor& t);
+// #endif
+
     bool       is_square() const;
+
+    tensor(std::vector<number_t> numbers): data(numbers), size() {};
 
     void set_tensor_size(const tensor_dim dim);
 };
